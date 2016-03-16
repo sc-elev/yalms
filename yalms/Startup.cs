2046cf7824a2f059e14740e043c5323b317c8273
John Doe <john.doe@nowhere.net>
2016-03-16 11:31:34 +0100

    Inital commit:  As created by VS

---
+++ yalms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(yalms.Startup))]
+namespace yalms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
