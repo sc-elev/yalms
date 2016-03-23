92a315fc07f900856b7e20ac9a0e8e302eb261fc
burncreek <burncreek@hotmail.com>
2016-03-23 16:24:11 +0100

    Intermediate checkin #3 for Swedish translations

--- yalms.Tests/Controllers/HomeControllerTest.cs
+++ yalms.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace yalms.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("yalms: Yet Another Learning Management System.", 
+            Assert.AreEqual("yalms | Yet Another Learning Management System", 
                             result.ViewBag.Message);
         }
 
