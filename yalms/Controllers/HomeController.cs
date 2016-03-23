490303f32d69b49008631e023eb1e08f624dac9f
burncreek <burncreek@hotmail.com>
2016-03-23 13:29:43 +0100

    Intermediate checkin for Swedish translations

--- yalms/Controllers/HomeController.cs
+++ yalms/Controllers/HomeController.cs
@@ -18,14 +18,14 @@ namespace yalms.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "yalms. Yet Another Learning Management System.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Alexander Wåhlin &bull; Michael Kolmodin &bull; Pekka Brännbäck";
 
             return View();
         }
