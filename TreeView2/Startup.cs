f362216ae244612e1580a6324d5df60bc057c8ba
John Doe <john.doe@nowhere.net>
2016-03-29 15:51:52 +0200

    Initial: hardcoded view.

---
+++ TreeView2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TreeView2.Startup))]
+namespace TreeView2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
