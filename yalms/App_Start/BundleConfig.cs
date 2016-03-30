7b2796ca4abc6794d2da2f4e7171d935a2d15745
John Doe <john.doe@nowhere.net>
2016-03-30 15:23:01 +0200

    StudentMainView: Static list of assignments.

--- yalms/App_Start/BundleConfig.cs
+++ yalms/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace yalms
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/json2.js",
+                        "~/Scripts/jstree.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ yalms/Scripts/StudentMainView.js
@@ -0,0 +1,20 @@
+﻿//
+
+$(function () {
+    $("#AssignmentsTree").jstree({
+        "plugins": ["themes", "html_data"]
+    });
+})
+.bind("select_node.jstree", function (e, data) {
+    var CurrObj = data.rslt.obj;
+    //Toggle on the click of that Node's name
+    $("#demo1").jstree("toggle_node", data.rslt.obj);
+})
+.bind("hover_node.jstree", function (e, data) {
+    //on hover
+    var nodeId = data.rslt.obj[0].id;
+})
+.bind("loaded.jstree", function (event, data) {
+    $(this).jstree("open_all");
+    alert("tree loaded");
+});
