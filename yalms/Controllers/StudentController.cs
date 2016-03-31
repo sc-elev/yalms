63599aa234a6719e9c8a53a81e10fdb31059045f
John dDe <John at cia dot com>
2016-03-31 15:38:36 +0200

    Controller, DAL: Refactor common UploadPaths DAL component.

--- yalms/Controllers/StudentController.cs
+++ yalms/Controllers/StudentController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using yalms.DAL;
 using yalms.Models;
 
 namespace yalms.Controllers
@@ -20,19 +21,6 @@ namespace yalms.Controllers
         protected EFContext context;
 
         protected StudentMainViewModelFactory modelFactory;
-
-        private void seedUploads()
-        {
-            string[] paths = new string[] {
-                "~/Upload", "~/Upload/Assignments", "~/Upload/Shared"};
-            foreach (var path in paths)
-            {
-                var dirpath =
-                    System.Web.HttpContext.Current.Server.MapPath(path);
-                if (Directory.Exists(dirpath)) Directory.Delete(dirpath, true);
-                Directory.CreateDirectory(dirpath);
-            }
-        }
 
      
         public ViewResult MainView()
@@ -84,7 +72,6 @@ namespace yalms.Controllers
         public ActionResult PostAssignment(HttpPostedFileBase assignmentFile, 
                                            int assignmentID)
         {
-            seedUploads();
             int id = assignmentID;
             StudentMainViewModel model = modelFactory.Create(null);
 
@@ -93,13 +80,8 @@ namespace yalms.Controllers
                 ViewBag.UploadMessage = "Fel: Ingenting laddades upp";
                 return View("MainView", model);
             }
-            string [] parts = {
-                 "~", "Upload", "Assignments", userProvider.UserID().ToString()};
-            var path = Path.Combine(parts);
-            path = System.Web.HttpContext.Current.Server.MapPath(path); //FIXME - testability.
-            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-            path = Path.Combine(
-                path, assignmentID.ToString() + "-" + assignmentFile.FileName);
+            var path = UploadPaths.GetAssignmentPath(
+                userProvider.UserID(), assignmentID, assignmentFile.FileName);
             string msg = "Filen " + assignmentFile.FileName + " uppladdad.";
             if (System.IO.File.Exists(path))
             {
---
+++ yalms/DAL/UploadPaths.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using yalms.Models;
+
+namespace yalms.DAL
+{
+    public class UploadPaths
+    {
+        // Danger zone: Re-initialize the Upload directory structure
+        public void seedUploads()
+        {
+            string[] paths = new string[] {
+                "~/Upload", "~/Upload/Assignments", "~/Upload/Shared"};
+            foreach (var path in paths)
+            {
+                var dirpath =
+                    System.Web.HttpContext.Current.Server.MapPath(path);
+                if (Directory.Exists(dirpath)) Directory.Delete(dirpath, true);
+                Directory.CreateDirectory(dirpath);
+            }
+        }
+
+
+        // Get path for given user + assignment, possibly creating missing 
+        // directories in path.
+        static public string GetAssignmentPath(
+            int userID, int assignmentID, string filename)
+        {
+            string [] parts = { 
+                 "~", "Upload", "Assignments", userID.ToString()};
+            var path = Path.Combine(parts);
+            path = System.Web.HttpContext.Current.Server.MapPath(path); //FIXME - testability.
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            return Path.Combine(
+                path, assignmentID.ToString() + "-" + filename);
+        }
+
+    }
+}
