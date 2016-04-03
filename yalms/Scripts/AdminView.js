036f4da17bebd6d7a1a396d27046398e09a4fa90
John dDe <John at cia dot com>
2016-04-03 11:40:40 +0200

    Admin: New view w tabs, skeletons for controller/model.

---
+++ yalms/Scripts/AdminView.js
@@ -0,0 +1,6 @@
+﻿$("#tabs").tabs({
+    activate: function (event, ui) {
+        var active = $('#tabs').tabs('option', 'active');
+        $("#tabid").html('the tab id is ' + $("#tabs ul>li a").eq(active).attr("href"));
+    }
+});
