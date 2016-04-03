8962d651448e493b20ec33ffd4063246556182a3
John dDe <John at cia dot com>
2016-04-03 10:10:12 +0200

    View, Controller: Implement stored docs in Student main view.

--- yalms/Scripts/StudentMainView.js
+++ yalms/Scripts/StudentMainView.js
@@ -1,10 +1,33 @@
 ﻿//
+
+function enableUploads() {
+    var hasAssignment = $("#assignmentId").val() != -1 &&
+        $("#assignmentFile").val().length > 4;
+    $('#assignmentUploadBtn').prop('disabled', !hasAssignment);
+    return true;
+};
+
+
 $(document).ready(function () {
     $("#AssignmentsTree").jstree({
         "plugins": ["themes", "html_data"]
     })
-    .on("select_node.jstree", function (e, data) {
-        var href = data.node.a_attr.href
+    .on('select_node.jstree', function (e, node) {
+        if (node.node.children.length == 0) {
+            $("#assignmentLabel").html(node.node.text);
+            $("#assignmentId").val(node.node.id);
+            $('#assignmentDownloadBtn').prop('disabled', false);
+            $("#uploadMsg").empty();
+            enableUploads();
+        }
+    })
+    $("#assignmentFile").on('change', function () { enableUploads() })
+
+    $("#submissionsTree").jstree({
+        "plugins": ["themes", "html_data"]
+    })
+    .on('select_node.jstree', function (e, node) {
+        var href = node.node.a_attr.href
         document.location.href = href;
     })
 });
