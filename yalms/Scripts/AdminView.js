1a27c0bf00c1d03f01cc353e87f91c6db58b1a03
John dDe <John at cia dot com>
2016-04-03 15:09:32 +0200

    View: User part OK.

--- yalms/Scripts/AdminView.js
+++ yalms/Scripts/AdminView.js
@@ -1,6 +1,1 @@
-﻿$("#tabs").tabs({
-    activate: function (event, ui) {
-        var active = $('#tabs').tabs('option', 'active');
-        $("#tabid").html('the tab id is ' + $("#tabs ul>li a").eq(active).attr("href"));
-    }
-});
+﻿
